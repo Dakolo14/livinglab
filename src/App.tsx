e0b25a923db034918091fb9343efeefb685e0e1c
Dakolo14 <Dakolo14@users.noreply.github.com>
2026-08-26 22:43:22 +0100

    Fix typescript unused variable errors for vercel build

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import HomeV1 from './pages/HomeV1';
 import HomeV2 from './pages/HomeV2';
