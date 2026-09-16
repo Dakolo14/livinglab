41900712f371babab4cd0dad27695c0ebcf358b1
Dakolo14 <Dakolo14@users.noreply.github.com>
2026-09-17 00:36:27 +0100

    Revert "feat: add SkinPathologies section after countdown"
    This reverts commit 33b04157ee5360bb8526c687bc9166799941d6bc.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import Header from '../components/layout/Header';
 import Hero from '../components/sections/Hero';
 import Countdown from '../components/sections/Countdown';
-import SkinPathologies from '../components/sections/SkinPathologies';
 import AboutLRP from '../components/sections/AboutLRP';
 import ConsultationReasons from '../components/sections/ConsultationReasons';
 import Partners from '../components/sections/Partners';
@@ -53,7 +52,6 @@ const Home: React.FC = () => {
       <main>
         <Hero activeReelId={activeReelId} isVideoMoved={isVideoMoved} />
         <Countdown />
-        <SkinPathologies />
         <AboutLRP />
         <ConsultationReasons />
         <Experiences activeReelId={activeReelId} setActiveReelId={setActiveReelId} isVideoMoved={isVideoMoved} />
