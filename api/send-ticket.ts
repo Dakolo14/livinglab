9d1c1dcb6f418bcb2acb1b454d4a26cd06028060
Dakolo14 <Dakolo14@users.noreply.github.com>
2026-09-25 08:43:59 +0100

    Update email logo to use WebP for faster loading

--- api/send-ticket.ts
+++ api/send-ticket.ts
@@ -225,7 +225,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     <div class="email-container">
       
       <div class="header-banner">
-        <img src="https://www.livinglabnigeria.com/BLUE%20LOGO.png" alt="Living Lab" class="logo" />
+        <img src="https://www.livinglabnigeria.com/livinglablogo.webp" alt="Living Lab" class="logo" />
         <h1 class="success-title">REGISTRATION<br/>SUCCESSFUL</h1>
       </div>
 
